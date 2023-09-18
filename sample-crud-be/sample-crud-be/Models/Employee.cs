db6d0a19102b93218abcb6e781d6a2c640f32029
Günay ŞEKERCİSOY <46304984+gsekercisoy@users.noreply.github.com>
2023-09-18 22:39:06 +0300

    Simple .NET 6 WEB API PROJECT

---
+++ sample-crud-be/sample-crud-be/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace sample_crud_be.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Age { get; set; }
+        public bool IsActive{ get; set; }
+
+    }
+}
